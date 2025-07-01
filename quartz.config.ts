63616c9113795c0fc1c32a2e09244fb23e1b4a75
M. Hosseyn Najafi <hosseynjf@icloud.com>
2025-07-01 21:08:45 +0300

    Quartz sync: Jul 1, 2025, 9:08 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "garden.hosseynjf.dev",
+    baseUrl: "hosseynjf.dev",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
